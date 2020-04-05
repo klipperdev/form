43321e46058247373cdb79e61cf30a82f0a1d2ac
François Pluchino <francois.pluchino@klipper.dev>
2020-04-05 10:58:21 +0200

    Add doctrine query builder transformer

---
+++ Doctrine/ChoiceList/QueryBuilderTransformer.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Component\Form\Doctrine\ChoiceList;
+
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+class QueryBuilderTransformer
+{
+    /**
+     * Get the query from the query builder with transformation.
+     */
+    public function getQuery(QueryBuilder $qb): Query
+    {
+        return $qb->getQuery();
+    }
+}
